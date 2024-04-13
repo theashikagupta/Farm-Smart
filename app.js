73047a5e98d0184979d7b630d9c898530c237793
Harshchauhan3104 <166842929+Harshchauhan3104@users.noreply.github.com>
2024-04-13 16:16:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+function clearSearch() {
+    document.getElementById('search-input').value = ''; // Clear the search input
+    document.getElementById('search-input').focus(); // Focus on the input after clearing
+}
